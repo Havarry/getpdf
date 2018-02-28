8622191ecf97474a16c69cbf3b4a3280e85f6bb5
Havarry <15622308894@163.com>
2018-02-28 23:57:39 +0800

    股票代码

--- download.py
+++ download.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding : UTF-8 -*-
+import http
 import random
+import socket
 
+import pandas as pd
 import requests
 import time
 
@@ -17,12 +20,52 @@ header = {
     }
 
 
+def get_proxy_ip():
+    # 代理服务器
+    proxyHost = "http-dyn.abuyun.com"
+    proxyPort = "9020"
+    # 代理隧道验证信息
+    proxyUser = "HJPC87609J7D516D"
+    proxyPass = "26D9C5391F9123CA"
+    proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
+        "host": proxyHost,
+        "port": proxyPort,
+        "user": proxyUser,
+        "pass": proxyPass,
+    }
+    proxies = {
+        "http": proxyMeta,
+        "https": proxyMeta,
+    }
+    return proxies
+
+
 # 获取pdf内容
 def get_content(purl):
-    time.sleep(0.5)  # 限制下载的频次速度，以免被封
+    timeout = random.choice(range(80, 180))
+    proxy = get_proxy_ip()
+    while True:
+        try:
+            req = requests.post(purl, headers=header, timeout=timeout, proxies=proxy, stream=True)
+            req.encoding = 'GBK'
+            break
+        except socket.timeout as e:
+            print('3:', e)
+            time.sleep(random.choice(range(2, 5)))
+
+        except socket.error as e:
+            print('4:', e)
+            time.sleep(random.choice(range(2, 6)))
+
+        except http.client.BadStatusLine as e:
+            print('5:', e)
+            time.sleep(random.choice(range(3, 5)))
+
+        except http.client.IncompleteRead as e:
+            print('6:', e)
+            time.sleep(random.choice(range(2, 4)))
     # NOTE the stream=True parameter
-    r = requests.post(purl, stream=True)
-    return r.iter_content(chunk_size=1024)
+    return req.iter_content(chunk_size=1024)
 
 
 # 将内容写入到本地pdf
@@ -35,22 +78,33 @@ def write_to_pdf(content):
 
 
 def get_code():
-    pass
+    path = '股票代码.xlsx'
+    all_data = pd.read_excel(path, converters={u'证券代码': str})
+    codes = all_data.get('证券代码')  # 证券代码
+    return codes
 
 
-def get_nianbao_url():
+def get_nianbao_url(stkcd):
     random_num = str(random.randint(10000, 99999))
-    code = ''
-    begin_year = ''
-    end_year = ''
+    code = stkcd
+    begin_year = '2002'
+    end_year = '2004'
     req_time = str(time.time())
     url = 'http://query.sse.com.cn/security/stock/queryCompanyStatementNew.do?jsonCallBack=jsonpCallback'+random_num + \
           '&isPagination=true&productId=' + code + '&keyWord=&isNew=1&reportType2=DQBG&reportType=YEARLY&beginDate=' + \
           begin_year + '-01-01&endDate=' + end_year + '-01-01&pageHelp.pageSize=25&pageHelp.pageCount=50' + \
           '&pageHelp.pageNo=1&pageHelp.beginPage=1&pageHelp.cacheSize=1&pageHelp.endPage=5&_=' + req_time
+    return url
 
 
 if __name__ == '__main__':
-    pdf_url = 'http://static.sse.com.cn/disclosure/listedinfo/announcement/c/2017-03-25/601999_2016_n.pdf'
-    write_to_pdf(get_content(pdf_url))
+    codes = get_code()
+    for code in codes:
+        url = get_nianbao_url(code)
+        print(url)
+        # break
+    # pdf_url = 'http://static.sse.com.cn/disclosure/listedinfo/announcement/c/2017-03-25/601999_2016_n.pdf'
+    # print(pdf_url)
+    # write_to_pdf(get_content(pdf_url))
+
 
